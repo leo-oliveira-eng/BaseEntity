bd94d7e919b327f261337e729a7b8aac1af481a2
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2020-09-03 03:59:34 -0300

    #1 - creating abstract class with basic properties

---
+++ BaseEntity.Domain/Entities/Extensions/EntityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BaseEntity.Domain.Entities.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static TEntity SetId<TEntity>(this TEntity entity, long id) where TEntity : Entity
+        {
+            entity.Id = id;
+            return entity;
+        }
+
+        public static TEntity SetCreatedAt<TEntity>(this TEntity entity, DateTime createdAt) where TEntity : Entity
+        {
+            entity.CreatedAt = createdAt;
+            return entity;
+        }
+
+        public static TEntity SetLastUpdate<TEntity>(this TEntity entity, DateTime lastUpdate) where TEntity : Entity
+        {
+            entity.LastUpdate = lastUpdate;
+            return entity;
+        }
+    }
+}
