1868643a52a13d373ad732e7207d61bc8ac0c043
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2020-09-04 01:42:44 -0300

    #6 - Including repositoy and uow contracts

---
+++ BaseEntity.Domain/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using BaseEntity.Domain.Entities;
+using Messages.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BaseEntity.Domain.Repositories
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<List<T>> GetAllAsync();
+
+        Task<Maybe<T>> FindAsync(Guid code);
+
+        Task AddAsync(T entity);
+
+        Task RemoveAsync(T entity);
+
+        Task UpdateAsync(T entity);
+    }
+}
---
+++ BaseEntity.Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace BaseEntity.Domain.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<bool> CommitAsync();
+    }
+}
